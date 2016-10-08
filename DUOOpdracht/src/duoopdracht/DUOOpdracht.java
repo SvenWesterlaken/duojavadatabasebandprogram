843dc578f3811ff16a97baffa3a401f5fea28509
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2016-10-08 16:43:48 +0200

    gitignore & organiseren

--- DUOOpdracht/src/duoopdracht/DUOOpdracht.java
+++ DUOOpdracht/src/duoopdracht/DUOOpdracht.java
@@ -25,17 +25,23 @@ public class DUOOpdracht {
     public static void main(String[] args) throws SQLException {
         HomeFrame HF = new HomeFrame();
         
-        
+        /*
+                      -- Maak Connectie met Database DUO_1 --
+                         Username: 'Root', Password: Geen
+            Root is het standaard 'administrator' account in de database
+        */
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/DUO_1", "root", "");
         Statement statement = connection.createStatement();
         ResultSet resultset = statement.executeQuery("SELECT * FROM band_artiest;");
         
+        /*
         while (resultset.next()) {
             int Sleutel = resultset.getInt("Sleutel");
             String Naam = resultset.getString("Naam");
             String Beschrijving = resultset.getString("Beschrijving");
             System.out.println(Sleutel + " " + Naam + " " + Beschrijving);
         }
+        */
     }
     
 }
--- DUOOpdracht/src/duoopdracht/HomePanel.java
+++ DUOOpdracht/src/duoopdracht/HomePanel.java
@@ -6,97 +6,109 @@ import duoopdracht.View.*;
 import javax.swing.*;
 import java.awt.event.*; 
 import javax.swing.border.*;
-import java.awt.Color;
 import java.awt.GridLayout;
 
 
 public class HomePanel extends JPanel{
     
+    JPanel HomePanel = new JPanel();
+    //Categorie Labels
+    private final JLabel labelbands, labelpodium, labeloptreden;
+    //Informatie Labels
+    private final JLabel labelnamen, labelschool;
+    //Band buttons
+    private final JButton btntoevoegen, btnverwijder, btnwijzig, btnview;
+    //Optreden buttons
+    private final JButton btntoevoegen2, btnverwijder2, btnwijzig2, btnview2;
+    //Podium buttons
+    private final JButton btntoevoegen3, btnverwijder3, btnwijzig3, btnview3;
     
- JPanel HomePanel = new JPanel();
- private final JLabel labelwelkom, labelnamen, labelbands, labelpodium, labeloptreden, labelschool, labelklas;
- private final JButton btntoevoegen, btntoevoegen2, btntoevoegen3 , btnverwijder ,btnverwijder2, btnverwijder3, 
- btnwijzig, btnwijzig2, btnwijzig3, btnview, btnview2, btnview3;
-     
-        public HomePanel(){   
-            labelwelkom = new JLabel("Welkom");
-            labelwelkom.setHorizontalAlignment(JButton.CENTER);
-
+    /**
+     * Constructor
+     */
+        public HomePanel(){ 
+            
+            //Set Layout & Add Border
+            this.setLayout (new GridLayout (4,6,5,5));
+            Border border = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+            this.setBorder(border);
+            
+            //Labels
             labelbands = new JLabel("bands");
-            labelbands.setHorizontalAlignment(JLabel.LEFT);
-
-            btntoevoegen = new JButton("toevoegen");
-            btntoevoegen.addActionListener(new OpenHandler());
-
-            btnverwijder = new JButton("verwijder");
-            btnverwijder.addActionListener(new OpenHandler());
-
-            btnwijzig = new JButton("wijzig");
-            btnwijzig.addActionListener(new OpenHandler());
-
-            btnview = new JButton("View");
-            btnview.addActionListener(new OpenHandler());
-            
-            
             labeloptreden = new JLabel("optreden");
-            labeloptreden.setHorizontalAlignment(JLabel.LEFT);
-
-
-            btntoevoegen2 = new JButton("toevoegen");
-            btntoevoegen2.addActionListener(new OpenHandler());
-
-            btnverwijder2 = new JButton("verwijder");
-            btnverwijder2.addActionListener(new OpenHandler());
-
-            btnwijzig2 = new JButton("wijzig");
-            btnwijzig2.addActionListener(new OpenHandler());
-
-            btnview2 = new JButton("View");
-            btnview2.addActionListener(new OpenHandler());
-
             labelpodium = new JLabel ("podium");
+            
+            labelnamen = new JLabel("Kevin en Sven, 23IVT1C");
+            labelschool = new JLabel("Avans Breda");
+            
+            //Align Labels Left
+            labelbands.setHorizontalAlignment(JLabel.LEFT);
+            labeloptreden.setHorizontalAlignment(JLabel.LEFT);
             labelpodium.setHorizontalAlignment(JLabel.LEFT);
 
+            //Band Buttons
+            btntoevoegen = new JButton("toevoegen");
+            btnverwijder = new JButton("verwijder");
+            btnwijzig = new JButton("wijzig");
+            btnview = new JButton("bekijk");
+            
+            //Add ActionListeners
+            btntoevoegen.addActionListener(new OpenHandler());
+            btnverwijder.addActionListener(new OpenHandler());
+            btnview.addActionListener(new OpenHandler());
+            btnwijzig.addActionListener(new OpenHandler());
+            
+            //Optreden Buttons
+            btntoevoegen2 = new JButton("toevoegen");
+            btnverwijder2 = new JButton("verwijder");
+            btnwijzig2 = new JButton("wijzig");
+            btnview2 = new JButton("bekijk");
+            
+            //Add ActionListeners
+            btntoevoegen2.addActionListener(new OpenHandler());
+            btnverwijder2.addActionListener(new OpenHandler());
+            btnwijzig2.addActionListener(new OpenHandler());
+            btnview2.addActionListener(new OpenHandler());
+
+            //Podium Buttons
             btntoevoegen3 = new JButton("toevoegen");
-            btntoevoegen3.addActionListener(new OpenHandler());
-
             btnverwijder3 = new JButton("verwijder");
-            btnverwijder3.addActionListener(new OpenHandler());
-
             btnwijzig3 = new JButton("wijzig");
+            btnview3 = new JButton("bekijk");
+            
+            //Add ActionListeners
+            btntoevoegen3.addActionListener(new OpenHandler());
+            btnverwijder3.addActionListener(new OpenHandler());
             btnwijzig3.addActionListener(new OpenHandler());
-
-            btnview3 = new JButton("View");
             btnview3.addActionListener(new OpenHandler());
 
-            labelnamen = new JLabel("Kevin en Sven");
-            labelklas = new JLabel("23IVT1C");
-            labelschool = new JLabel("Avans Breda");
+            //Add All Labels & Buttons
+            
+            //Bands
+            this.add(labelbands);
+            this.add(btntoevoegen);
+            this.add(btnverwijder);
+            this.add(btnwijzig);
+            this.add(btnview);
 
-            add(labelbands);
-            add(btntoevoegen);
-            add(btnverwijder);
-            add(btnwijzig);
-            add(btnview);
+            //Optreden
+            this.add(labeloptreden);
+            this.add(btntoevoegen2);
+            this.add(btnverwijder2);
+            this.add(btnwijzig2);
+            this.add(btnview2);
 
-            add(labeloptreden);
-            add(btntoevoegen2);
-            add(btnverwijder2);
-            add(btnwijzig2);
-            add(btnview2);
+            //Podium
+            this.add(labelpodium);
+            this.add(btntoevoegen3);
+            this.add(btnverwijder3);
+            this.add(btnwijzig3);
+            this.add(btnview3);
 
-
-            add(labelpodium);
-            add(btntoevoegen3);
-            add(btnverwijder3);
-            add(btnwijzig3);
-            add(btnview3);
-
-            add(labelnamen);
-            add(labelklas);
-            add(labelschool);
-                  
-        setLayout (new GridLayout (4,6,8,8));
+            //Info
+            this.add(labelnamen);
+            this.add(labelschool);
+           
     }
         class OpenHandler implements ActionListener {
         
